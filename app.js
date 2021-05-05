6e44fc470495324d357dc174825b803b3c195d97
michealccc <4558199+michealccc@users.noreply.github.com>
2021-05-05 14:25:47 +1200

    Formatting

--- app.js
+++ app.js
@@ -15,15 +15,15 @@ var playersRouter = require('./routes/players')
 
 var app = express();
 
-app.use(cors({ origin: true, credentials: true }));
-app.use(express.json({ extended: false }));
+app.use(cors({origin: true, credentials: true}));
+app.use(express.json({extended: false}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -35,19 +35,19 @@ app.use('/scores', scoresRouter);
 connectDB();
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
